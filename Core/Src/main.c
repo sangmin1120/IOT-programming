d3956fa596f260958619f8345dc94396a6941c8a
sangmin1120 <sangmin8276@gmail.com>
2024-10-25 17:23:04 +0900

    feat: uart 설정
    터미널로 출력

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -21,7 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include <stdio.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -56,7 +56,10 @@ static void MX_USART2_UART_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
+int __io_putchar(int ch){
+	HAL_UART_Transmit(&huart2, (uint8_t)&ch,1,HAL_MAX_DELAY);
+	return ch;
+}
 /* USER CODE END 0 */
 
 /**
